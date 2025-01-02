69400fd6c063cff9f39b037b87ca70cbc27ffca1
suuuuzy <jangsooji91@gmail.com>
2025-01-02 20:55:10 +0900

    함수를 사용한 비즈니스 로직 구현

---
+++ src/main/java/com/polarbookshop/dispatcherservice/DispatchingFunctions.java
@@ -0,0 +1,26 @@
+package com.polarbookshop.dispatcherservice;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
+
+import java.util.function.Function;
+
+public class DispatchingFunctions {
+
+    private static final Logger log = LoggerFactory.getLogger(DispatchingFunctions.class);
+
+    public Function<OrderAcceptedMessage, Long> pack() {
+        return orderAcceptedMessage -> {
+            log.info("The order with id {} is packed.", orderAcceptedMessage.orderId());
+            return orderAcceptedMessage.orderId();
+        };
+    }
+
+    public Function<Flux<Long>, Flux<OrderDispatchedMessage>> label() {
+        return orderFlux -> orderFlux.map(orderId -> {
+            log.info("The order with id {} is labeled.", orderId);
+            return new OrderDispatchedMessage(orderId);
+        });
+    }
+}
